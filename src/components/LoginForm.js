13b0c4765113752c32b9497e43a14bd4ce20c7e8
ParkSeongJun <sjyj98@naver.com>
2023-10-31 12:17:07 +0900

    회원가입 정보 저장

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,4 +1,6 @@
 import { useState, useRef } from "react";
+import { Link, useSearchParams } from "react-router-dom";
+import axios from "axios";
 
 import classes from "./LoginForm.module.css";
 
@@ -6,14 +8,64 @@ import classes from "./LoginForm.module.css";
 const LoginForm = () => {
     const emailInputRef = useRef();
     const passwordInputRef = useRef();
+    const nameInputRef = useRef();
 
     const [isValid, setIsValid] = useState(true);
+    const [searchParams] = useSearchParams();
+    const isLogin = searchParams.get("mode") === "login";
+
+
+    const postSignUp = async (email, password, namein) => {
+        const url = "https://mylisttest-be9b5-default-rtdb.firebaseio.com/users.json";
+        try {
+            await axios.post(url, {
+                id:email,
+                password:password,
+                name:namein
+            });
+        } catch(err) {
+            console.log(err);
+        }
+    }
+
+    const signinHandler = (event) => {
+        event.preventDefault();
+
+        const enteredEmail = emailInputRef.current.value;
+        const enteredPassword = passwordInputRef.current.value;
+        const enteredName = nameInputRef.current.value;
+
+        if(enteredEmail.trim().length === 0 || enteredPassword.trim().length === 0) {
+            setIsValid(false);
+            return;
+        }
+
+        postSignUp(enteredEmail, enteredPassword, enteredName);
+        window.alert('회원가입이 완료되었습니다.');
+        emailInputRef.current.value = '';
+        passwordInputRef.current.value = '';
+        nameInputRef.current.value = '';
+
+    };
+
+    const loginHandler = () =>{};
+
+    const finbtnHandler = () => {
+        if(!isLogin) {
+            signinHandler();
+        } else {
+            loginHandler();
+        }
+    };
 
     
     
         return (
                 <div className={classes.formdiv}>
-                    <form className={classes.forms}>
+                    <Link to={`?mode=${isLogin ? 'signup' : 'login'}`} className={classes.btnlink}>
+                        {isLogin ? 'Go to Signup' : 'Go to Login'}
+                    </Link>
+                    <div className={classes.forms}>
                         <div className={classes.idzone}>
                             <label htmlFor="id">Email</label>
                             <input type="text" placeholder="email" ref={emailInputRef} />
@@ -22,11 +74,19 @@ const LoginForm = () => {
                             <label htmlFor="password">Password</label>
                             <input type="password" placeholder="password" ref={passwordInputRef} />
                         </div>
+                        {
+                        !isLogin && 
+                            <div className={classes.namezone}>
+                                <label htmlFor="name">name</label>
+                                <input type="text" placeholder="name" ref={nameInputRef} />
+                            </div>
+                        }
                         <div className={classes.buttonzone}>
-                            <button type="button">Login</button>
-                            <button type="button">Sign Up</button>
+                            <div className={classes.finbtn}>
+                                <button type="button" onClick={signinHandler}>{isLogin ? 'Login' : 'signup'}</button>
+                            </div>
                         </div>
-                    </form>
+                    </div>
                 </div>
         );
 };
